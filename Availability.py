0a00630c5eb5e1719a92562a271ef7af7334ed36
rajeshnadiminti <rajesh.nadiminti@clearballot.com>
2018-08-08 15:48:42 -0400

    Updated with UI for User Input
    Now when you run the code,  user will be prompted with a editor windows asking to enter the Server hostname or IP Address

--- Availability.py
+++ Availability.py
@@ -1,8 +1,31 @@
 import requests
 import time
 import smtplib
+from tkinter import *
+from tkinter import messagebox
 
-def web_site_online(url='https://google.com/', timeout=5):
+def clicked():
+    global url, s
+    s = url.get()
+    if len(s) == 0:
+        messagebox.showerror('Error..!', 'No Entry. Please Enter Valid Hostname or IP Address')
+    else:
+        messagebox.showinfo('Hostname','Input Successfull')
+        input.destroy()
+
+input = Tk()
+Label(input, text="Enter the HostName or IP Address").grid(row=0)
+url = Entry(input)
+url.grid(row=0, column=1)
+
+Button(input, text='Submit', command=clicked).grid(row=2, column=1)
+
+input.mainloop()
+
+
+print (s)
+
+def web_site_online(s, timeout=5):
 
     server = smtplib.SMTP(host='smtp.gmail.com', port=587) #Please make sure to change th Gmail settings to allow the less secure apps
     server.starttls()
@@ -14,7 +37,7 @@ def web_site_online(url='https://google.com/', timeout=5):
     msg = "Server is Down..!"
 
     try:
-        req = requests.get(url, timeout=timeout)
+        req = requests.get(s, timeout=timeout)
         # HTTP errors are not raised by default, this statement does that
         req.raise_for_status()
         print("connection successful")
